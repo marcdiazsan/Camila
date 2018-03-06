c07c2286e305cbd315feeb530c8fee1acab4d675
Camila Diaz <marcdiazsan@unal.edu.co>
2018-03-06 17:06:20 -0500

    ksnd

--- exponencial.cpp
+++ exponencial.cpp
@@ -1,14 +1,41 @@
 #include<iostream>
 #include<cmath>
 
+double factorial (double n);
+double termino (double n,double m);
 
 int main()
-{double t,suma,n;
+{
+  std::cout.precision(16);
+  std::cout.setf(std::ios::scientific);
+  double t,suma,n,num;
+  num=3.14159;
   n=0;
-  while(t>= 1*e-6)
-    {
-      t=termino(n);
-      suma+=termino;
-
+  suma=0;
+  do
+    { 
+      t=termino(n,num);
+      suma+=t;
+      std::cout<<n<<"\t"<<suma<<"\t"<<t<<std::endl;
+      n+=1;
     }
+  while(std::abs(t)>=(std::pow(10,-6)));
+}
+
+double factorial (double n)
+{
+  double i;
+  if (n==1||n==0)
+    {i=1;}
+  else
+    {i=n*factorial(n-1);}
+  return i;
+}
+
+double termino (double n,double m)
+{
+  double valor;
+  valor=(std::pow(m,n))/(factorial(n));
+  return valor;
+  
 }
