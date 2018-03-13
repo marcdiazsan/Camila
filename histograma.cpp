dff11e30d7bd85a8c74aaa90fea34b489c9cfbca
Camila Diaz <marcdiazsan@unal.edu.co>
2018-03-13 14:49:40 -0500

    histograma corregido

--- histograma.cpp
+++ histograma.cpp
@@ -3,13 +3,16 @@
 #include<cmath>
 const double xi=1;
 const double xf=2;
+const double xmed=1.5;
+const double sigma=0.3;
+const int n=20;
+const int p=100;
 int main(void)
 {   
-  int seed,n;
-  n=10;
+  int seed;
   double d,delta,a,x[n];
   delta=(xf-xi)/n;
-  int s[n];
+  double s[n];
   x[0]=xi;
   for(int i=0; i<n; i++)
     {
@@ -18,11 +21,11 @@ int main(void)
     }
   seed=5;	
   std::mt19937 gen(seed);	
-  std::uniform_real_distribution<>dis(xi,xf);
-  for(int m=1; m<=10000;++m)
+  std::normal_distribution<>dis(xmed,sigma);
+  for(int m=1; m<=p;++m)
     { 
       d=dis(gen);
-      // std::cout<<d<<std::endl;
+      //std::cout<<d<<std::endl;
       for(int j=0;j<n;j++)
 	{
 	  if((d-x[j])<delta)
@@ -35,7 +38,7 @@ int main(void)
     }
   for(int k=0;k<=n-1;k++)
     {
-      std::cout<<x[k]<<"-"<<x[k+1]<<"\t"<<s[k]-s[k-1]<<std::endl;
+      std::cout<<x[k]<<"-"<<x[k+1]<<"\t"<<(s[k]-s[k-1])/p<<std::endl;
     }
     
  
